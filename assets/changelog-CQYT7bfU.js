52e898742fe4df876f1e5243433e46acbd56d0e4
Dean Yang <dean@biji.com>
2026-03-19 15:51:04 +0800

    Updates

---
+++ assets/changelog-CQYT7bfU.js
@@ -0,0 +1,41 @@
+const n={frontmatter:{title:"版本更新",hidden:!1,toc:!0,draft:!1},html:`<h1 id="版本更新"><a class="heading-anchor" aria-hidden="" tabindex="-1" href="#版本更新"><span class="icon icon-link"></span></a>版本更新</h1>
+<p>Get笔记 各平台更新记录。</p>
+<hr>
+<h2 id="ios-版本更新"><a class="heading-anchor" aria-hidden="" tabindex="-1" href="#ios-版本更新"><span class="icon icon-link"></span></a><a href="/changelog/ios">iOS 版本更新</a></h2>
+<p>最新版本：v2.6.5 | App Store 下载</p>
+<hr>
+<h2 id="android-版本更新"><a class="heading-anchor" aria-hidden="" tabindex="-1" href="#android-版本更新"><span class="icon icon-link"></span></a><a href="/changelog/android">Android 版本更新</a></h2>
+<p>最新版本：v2.6.5 | 应用宝、华为、小米等渠道下载</p>
+<hr>
+<h2 id="harmonyos-版本更新"><a class="heading-anchor" aria-hidden="" tabindex="-1" href="#harmonyos-版本更新"><span class="icon icon-link"></span></a><a href="/changelog/harmonyos">HarmonyOS 版本更新</a></h2>
+<p>最新版本：v2.6.5 | 华为应用市场下载</p>
+<hr>
+<blockquote>
+<p>💡 想要什么新功能？可以在 App 内反馈给我们。</p>
+</blockquote>`,headings:[{depth:2,text:"iOS 版本更新",id:"ios-版本更新"},{depth:2,text:"Android 版本更新",id:"android-版本更新"},{depth:2,text:"HarmonyOS 版本更新",id:"harmonyos-版本更新"}],raw:`
+# 版本更新
+
+Get笔记 各平台更新记录。
+
+---
+
+## [iOS 版本更新](/changelog/ios)
+
+最新版本：v2.6.5 | App Store 下载
+
+---
+
+## [Android 版本更新](/changelog/android)
+
+最新版本：v2.6.5 | 应用宝、华为、小米等渠道下载
+
+---
+
+## [HarmonyOS 版本更新](/changelog/harmonyos)
+
+最新版本：v2.6.5 | 华为应用市场下载
+
+---
+
+> 💡 想要什么新功能？可以在 App 内反馈给我们。
+`};export{n as default};
