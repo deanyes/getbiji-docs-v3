b789d835e92d33d9717b62689dee431bb86be198
Dean Yang <dean@biji.com>
2026-03-19 15:52:14 +0800

    Updates

--- assets/changelog-Cl8qN1c2.js
+++ assets/changelog-Cl8qN1c2.js
@@ -1,13 +1,13 @@
 const n={frontmatter:{title:"版本更新",hidden:!1,toc:!0,draft:!1},html:`<h1 id="版本更新"><a class="heading-anchor" aria-hidden="" tabindex="-1" href="#版本更新"><span class="icon icon-link"></span></a>版本更新</h1>
 <p>Get笔记 各平台更新记录。</p>
 <hr>
-<h2 id="ios-版本更新"><a class="heading-anchor" aria-hidden="" tabindex="-1" href="#ios-版本更新"><span class="icon icon-link"></span></a><a href="/changelog/ios">iOS 版本更新</a></h2>
+<h2 id="ios-版本更新"><a class="heading-anchor" aria-hidden="" tabindex="-1" href="#ios-版本更新"><span class="icon icon-link"></span></a><a href="/getbiji-docs-v3/changelog/ios">iOS 版本更新</a></h2>
 <p>最新版本：v2.6.5 | App Store 下载</p>
 <hr>
-<h2 id="android-版本更新"><a class="heading-anchor" aria-hidden="" tabindex="-1" href="#android-版本更新"><span class="icon icon-link"></span></a><a href="/changelog/android">Android 版本更新</a></h2>
+<h2 id="android-版本更新"><a class="heading-anchor" aria-hidden="" tabindex="-1" href="#android-版本更新"><span class="icon icon-link"></span></a><a href="/getbiji-docs-v3/changelog/android">Android 版本更新</a></h2>
 <p>最新版本：v2.6.5 | 应用宝、华为、小米等渠道下载</p>
 <hr>
-<h2 id="harmonyos-版本更新"><a class="heading-anchor" aria-hidden="" tabindex="-1" href="#harmonyos-版本更新"><span class="icon icon-link"></span></a><a href="/changelog/harmonyos">HarmonyOS 版本更新</a></h2>
+<h2 id="harmonyos-版本更新"><a class="heading-anchor" aria-hidden="" tabindex="-1" href="#harmonyos-版本更新"><span class="icon icon-link"></span></a><a href="/getbiji-docs-v3/changelog/harmonyos">HarmonyOS 版本更新</a></h2>
 <p>最新版本：v2.6.5 | 华为应用市场下载</p>
 <hr>
 <blockquote>
@@ -19,19 +19,19 @@ Get笔记 各平台更新记录。
 
 ---
 
-## [iOS 版本更新](/changelog/ios)
+## [iOS 版本更新](/getbiji-docs-v3/changelog/ios)
 
 最新版本：v2.6.5 | App Store 下载
 
 ---
 
-## [Android 版本更新](/changelog/android)
+## [Android 版本更新](/getbiji-docs-v3/changelog/android)
 
 最新版本：v2.6.5 | 应用宝、华为、小米等渠道下载
 
 ---
 
-## [HarmonyOS 版本更新](/changelog/harmonyos)
+## [HarmonyOS 版本更新](/getbiji-docs-v3/changelog/harmonyos)
 
 最新版本：v2.6.5 | 华为应用市场下载
 
